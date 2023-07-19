b6c5bebe685754dd7371e563b20ed70a42a6ab60
umut <umut_baykan@hotmail.com>
2023-07-19 23:06:41 +0300

    item class to throw errors if data type is incorrect

--- test/item.test.js
+++ test/item.test.js
@@ -2,8 +2,16 @@ const Item = require('../lib/item');
 
 describe("Item class", () => {
   it("initializes with a name and price", () => {
-    item = new Item("Muffin", 20)
+    const item = new Item("Muffin", 20)
     expect(item.name).toEqual("Muffin")
     expect(item.price).toEqual(20)
+  })
+
+  it("fails to initialize with invalid data type for price", () => {
+    expect(() => new Item("Muffin", [])).toThrow("Invalid data.")
+  })
+
+  it("fails to initialize with invalid data type for name", () => {
+    expect(() => new Item(false, 20)).toThrow("Invalid data.")
   })
 })
