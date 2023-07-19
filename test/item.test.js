86a303050319e48468881a405a9459dca572ee2b
umut <umut_baykan@hotmail.com>
2023-07-19 23:08:22 +0300

    added a script to prettify js files

--- test/item.test.js
+++ test/item.test.js
@@ -1,17 +1,17 @@
-const Item = require('../lib/item');
+const Item = require("../lib/item");
 
 describe("Item class", () => {
   it("initializes with a name and price", () => {
-    const item = new Item("Muffin", 20)
-    expect(item.name).toEqual("Muffin")
-    expect(item.price).toEqual(20)
-  })
+    const item = new Item("Muffin", 20);
+    expect(item.name).toEqual("Muffin");
+    expect(item.price).toEqual(20);
+  });
 
   it("fails to initialize with invalid data type for price", () => {
-    expect(() => new Item("Muffin", [])).toThrow("Invalid data.")
-  })
+    expect(() => new Item("Muffin", [])).toThrow("Invalid data.");
+  });
 
   it("fails to initialize with invalid data type for name", () => {
-    expect(() => new Item(false, 20)).toThrow("Invalid data.")
-  })
-})
+    expect(() => new Item(false, 20)).toThrow("Invalid data.");
+  });
+});
