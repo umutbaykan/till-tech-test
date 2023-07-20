9af6bb657f6325305d338e939e5b0f8bf5e48c73
umut <umut_baykan@hotmail.com>
2023-07-20 15:58:05 +0300

    amended existing till class to be defined of static methods only

--- test/till.test.js
+++ test/till.test.js
@@ -9,52 +9,39 @@ describe("Till class", () => {
     mock_coffee = { name: "Coffee", price: 10.45 };
   });
 
-  test("initializes with an empty basket and discounts", () => {
-    const till = new Till();
-    expect(till.basket).toEqual([]);
-    expect(till.discounts).toEqual({});
-  });
-
-  test("initializes with objects if passed in", () => {
-    const someDiscount = () => {};
-    const till = new Till([mock_muffin], { someDiscount });
-    expect(till.basket).toEqual([mock_muffin]);
-    expect(till.discounts).toEqual({ someDiscount });
-  });
-
   describe("calculate total method", () => {
     test("calculates the totals of the items in the basket", () => {
-      const till = new Till([mock_coffee, mock_muffin, mock_muffin], {});
-      expect(till.calculateTotal()).toEqual(50.45);
+      expect(
+        Till.calculateTotal([mock_coffee, mock_muffin, mock_muffin]),
+      ).toEqual(50.45);
     });
 
     test("returns 0 if nothing is in basket", () => {
-      const till = new Till();
-      expect(till.calculateTotal()).toEqual(0);
+      expect(Till.calculateTotal([])).toEqual(0);
     });
   });
 
   describe("calculate tax method", () => {
     test("calculates the total tax of items in the basket before reductions", () => {
-      const till = new Till([mock_coffee, mock_muffin]);
-      expect(till.calculateTax()).toEqual(3.52);
+      expect(Till.calculateTax([mock_coffee, mock_muffin], 8.64)).toEqual(3.52);
     });
 
     test("returns 0 if nothing is in basket", () => {
-      const till = new Till();
-      expect(till.calculateTax()).toEqual(0);
+      expect(Till.calculateTax([], 8.64)).toEqual(0);
     });
   });
 
   describe("calculate change method", () => {
     test("returns the change customer will get", () => {
-      const till = new Till([mock_coffee, mock_muffin]);
-      expect(till.calculateChange(50)).toEqual(16.03);
+      expect(
+        Till.calculateChange([mock_coffee, mock_muffin], 50, 8.64),
+      ).toEqual(16.03);
     });
 
     test("throws an error if the amount is less than total", () => {
-      const till = new Till([mock_coffee, mock_muffin]);
-      expect(() => till.calculateChange(30)).toThrow("Insufficient funds.");
+      expect(() =>
+        Till.calculateChange([mock_coffee, mock_muffin], 30, 8.64),
+      ).toThrow("Insufficient funds.");
     });
   });
 });
