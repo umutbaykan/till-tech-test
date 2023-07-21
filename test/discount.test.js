75ab2dfd1cbe6599dbe58b9b3fd3f4867911e392
umut <umut_baykan@hotmail.com>
2023-07-21 12:49:22 +0300

    class extension for specific discounts that can be applied to different conditions

--- test/discount.test.js
+++ test/discount.test.js
@@ -1,11 +1,23 @@
 const Discount = require("../lib/discount");
 
+class MuffinDiscount extends Discount {
+  constructor() {
+    super("percentage", 10, "10% off from muffins if you buy three");
+  }
+  checkApplicable = (basket) => {
+    const filteredList = basket.filter((item) => item.name === "Muffin");
+    if (filteredList.length > 2) {
+      return true;
+    } else return false;
+  };
+}
+
 describe("Discount class", () => {
   it("initializes with type, value and description constructors", () => {
     const discount = new Discount("fixed", 2, "2 dollars off");
     expect(discount.type).toEqual("fixed");
     expect(discount.value).toEqual(2);
-    expect(discount.description).toEqual("2 dollars off")
+    expect(discount.description).toEqual("2 dollars off");
   });
 
   it("throws an error if discount type is neither fixed nor percentage", () => {
@@ -15,7 +27,26 @@ describe("Discount class", () => {
   });
 
   it("has a discount condition which return true by default", () => {
-    const discount = new Discount("fixed", 3)
-    expect(discount.discountCondition()).toEqual(true)
-  })
+    const discount = new Discount("fixed", 3);
+    expect(discount.checkApplicable()).toEqual(true);
+  });
+});
+
+describe("Discount instances", () => {
+  test("creates a specific condition for the discount to be applied", () => {
+    const basket = [{ name: "Muffin" }, { name: "Muffin" }, { name: "Muffin" }];
+    const muffinDiscount = new MuffinDiscount();
+    expect(muffinDiscount.type).toEqual("percentage");
+    expect(muffinDiscount.value).toEqual(10);
+    expect(muffinDiscount.description).toEqual(
+      "10% off from muffins if you buy three",
+    );
+    expect(muffinDiscount.checkApplicable(basket)).toEqual(true);
+  });
+
+  test("returns false if the discount condition is not met", () => {
+    const basket = [{ name: "Coffee" }, { name: "Muffin" }, { name: "Muffin" }];
+    const muffinDiscount = new MuffinDiscount();
+    expect(muffinDiscount.checkApplicable(basket)).toEqual(false);
+  });
 });
