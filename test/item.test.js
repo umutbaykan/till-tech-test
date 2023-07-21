f6b710be367c26deeee7dad9a7db2ecaf23026da
umut <umut_baykan@hotmail.com>
2023-07-21 11:28:32 +0300

    added discount attribute to item class

--- test/item.test.js
+++ test/item.test.js
@@ -5,6 +5,7 @@ describe("Item class", () => {
     const item = new Item("Muffin", 20);
     expect(item.name).toEqual("Muffin");
     expect(item.price).toEqual(20);
+    expect(item.discount).toEqual(null);
   });
 
   it("fails to initialize with invalid data type for price", () => {
@@ -13,5 +14,11 @@ describe("Item class", () => {
 
   it("fails to initialize with invalid data type for name", () => {
     expect(() => new Item(false, 20)).toThrow("Invalid data.");
+  });
+
+  it("can be added a discount", () => {
+    const item = new Item("Muffin", 20);
+    item.discount = { type: "fixed", percentage: 2 };
+    expect(item.discount).toEqual({ type: "fixed", percentage: 2 });
   });
 });
