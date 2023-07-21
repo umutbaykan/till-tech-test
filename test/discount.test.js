0ed617225954dd0f950a486e06b223277bd9e11a
umut <umut_baykan@hotmail.com>
2023-07-21 12:32:23 +0300

    discount class to have condition method

--- test/discount.test.js
+++ test/discount.test.js
@@ -1,10 +1,11 @@
 const Discount = require("../lib/discount");
 
 describe("Discount class", () => {
-  it("initializes with type and value constructors", () => {
-    const discount = new Discount("fixed", 2);
+  it("initializes with type, value and description constructors", () => {
+    const discount = new Discount("fixed", 2, "2 dollars off");
     expect(discount.type).toEqual("fixed");
     expect(discount.value).toEqual(2);
+    expect(discount.description).toEqual("2 dollars off")
   });
 
   it("throws an error if discount type is neither fixed nor percentage", () => {
@@ -12,4 +13,9 @@ describe("Discount class", () => {
       "Type must be percentage or fixed!",
     );
   });
+
+  it("has a discount condition which return true by default", () => {
+    const discount = new Discount("fixed", 3)
+    expect(discount.discountCondition()).toEqual(true)
+  })
 });
